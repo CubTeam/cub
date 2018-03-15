cdee70eb575586e6825a43319ba31fa2f45315c9
三石兄 <huangsanshi@users.noreply.github.com>
2018-03-15 18:41:47 +0800

    2018-3-15

--- application/admin/common.php
+++ application/admin/common.php
@@ -8,7 +8,7 @@ class Common extends Controller
 {
     public function initialize(){
 
-     if(!session('id') || !session('name')){
+     if(!session('admin_id') || !session('admin_name')){
         $this->error('您尚未登录系统',url('login/index'));
      }
 
